e70ce995282ee8af36788b68377cf97b25d17254
Kyriakos Kritikos <domcretan@gmail.com>
2021-11-26 01:38:52 +0200

    Main removal
    Removed main class from MathParser as testing classes will cover its content soon

--- src/main/java/math/MathParser.java
+++ src/main/java/math/MathParser.java
@@ -34,21 +34,4 @@ public class MathParser {
 		}
 		return expr.calculate();
 	}
-	
-	public static void main(String[] args) {
-		boolean res = MathParser.syntaxCheck("x + ");
-		if (!res) System.out.println("Expression: \"x + \" is wrong!!!");
-		
-		try {
-			Map<String,Double> map = new HashMap<String,Double>();
-			map.put("x",2.0);
-			map.put("y",3.0);
-			double result = MathParser.expressionEvaluation("x + y", map);
-			System.out.println("x + y = " + result);
-			
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
